057ec580f599c5a127cd0a44431019c1cdcdd6c4
KwyTechDev <KwyTechDev@users.noreply.github.com>
2024-07-24 02:07:56 +0000

    deploy: 96ca7fc9b94d05c9f04c83aed8617eae5d670cd9

--- cypress/e2e/main_article_check.cy.js
+++
@@ -1,8 +0,0 @@
-describe('Record and Play Hubtel', () => {
-    // Main Article Check: Verify the main article exists
-    it('Main Article Check', function() {
-      cy.visit('https://blog.hubtel.com/');
-      cy.get('.text-decoration-none > .h-100').should('exist'); // Verify the main article exists
-    });
-  });
-  
